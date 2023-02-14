58413f1daf7d39cb1463a0c3f3f8271e6c6eae2a
JamieH2 <115276813+JamieH2@users.noreply.github.com>
2023-02-14 14:22:48 +0000

    small committing test (changing the title on the page)

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Time Crunch'),
+      home: const MyHomePage(title: 'Time crunch'),
     );
   }
 }
