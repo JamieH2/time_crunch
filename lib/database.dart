4f7133d257ff7e99d6f0d5ccdd61431191cf7216
lliot-camacho <UP2069042@myport.ac.uk>
2023-03-06 17:40:08 +0000

    Added a database.dart file in the lib folder that we're gonna build the database in, along with updating the yaml to work with hive

--- lib/database.dart
+++ lib/database.dart
@@ -1,5 +1,5 @@
 //This is where we're gonna build the database
-//Then it can be imported by the main and slotted into the front stuff
+//Then it can be imported by the main and slotted into the front stuff.
 
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
