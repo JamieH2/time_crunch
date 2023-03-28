26348b03f3a94005f9791442c5f39dd01b7dd422
lliot-camacho <UP2069042@myport.ac.uk>
2023-03-28 11:18:49 +0100

    Elliot and Jamie's constructed database + Elliot's crud demo + flutter modular nav bar found by Aman and merged by Elliot.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,17 @@ import 'package:hive_flutter/hive_flutter.dart';
 //import 'package:path_provider/path_provider.dart' as path_provider;   //pretty sure we don't need this but i'll keep it
 import 'settings.dart';
 
+//imports for the screens files Elliot's moving over from that github tutorial
+import 'package:time_crunch/screens/home_page.dart';
+import 'package:time_crunch/screens/todo_page.dart';
+import 'package:time_crunch/screens/email_page.dart';
+import 'package:time_crunch/screens/meeting_page.dart';
+import 'package:time_crunch/screens/news_page.dart';
+import 'package:time_crunch/screens/tutorial_page.dart';
+import 'package:time_crunch/screens/settings_page.dart';
+
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();   //HIVE SETUP
@@ -52,112 +63,85 @@ void main() async{
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-    // Hello- Test
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'test Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Time crunch'),
+      debugShowCheckedModeBanner: false,
+      home: CustomizedBottomNavigationbar(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CustomizedBottomNavigationbar extends StatefulWidget {
+  CustomizedBottomNavigationbar({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CustomizedBottomNavigationbarState createState() =>
+      _CustomizedBottomNavigationbarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _CustomizedBottomNavigationbarState
+    extends State<CustomizedBottomNavigationbar> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: bodySection(),
+      bottomNavigationBar: customBottomNavigationBar(),
+    );
+  }
+
+// * Body Section Components
+  bodySection() {
+    switch (index){
+      case 0:
+        return HomePage();
+      case 1 :
+        return ToDoPage();
+      case 2 :
+        return EmailPage();
+      case 3:
+        return MeetingPage();
+
+
+    }
+  }
+
+// * BottomNavigationBar Section Components
+  customBottomNavigationBar() {
+    return BottomNavyBar(
+      selectedIndex: index,
+      backgroundColor: Colors.blue,
+      items: [
+        BottomNavyBarItem(
+          icon: Icon(Icons.home_outlined),
+          title: Text('Home'),
+          activeColor: Colors.white,
+          textAlign: TextAlign.center,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        BottomNavyBarItem(
+            icon: Icon(Icons.check_circle_outline_rounded),
+            title: Text('ToDo'),
+            activeColor: Colors.white,
+            textAlign: TextAlign.center),
+        BottomNavyBarItem(
+          icon: Icon(Icons.email_outlined),
+          title: Text('Email'),
+          activeColor: Colors.white,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: Icon(Icons.meeting_room_outlined),
+          title: Text('Meeting'),
+          activeColor: Colors.white,
+          textAlign: TextAlign.center,
+        ),
+
+      ],
+      onItemSelected: (index) => setState(() => this.index = index),
     );
   }
 }
