e703a5250c73af252cbb733f6466f1706c2a1bc3
lliot-camacho <UP2069042@myport.ac.uk>
2023-03-28 11:21:07 +0100

    Elliot and Jamie's constructed database + Elliot's crud demo + flutter modular nav bar found by Aman and merged by Elliot.

---
+++ lib/screens/tutorial_page.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class TutorialPage extends StatelessWidget {
+  const TutorialPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.blue,
+        title: Column(
+          children: [
+            Text("Time Crunch",
+                style: TextStyle(fontSize: 15, color: Colors.white))
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.settings,
+              size: 120,
+              color: Colors.blue,
+            ),
+            Text("Settings", style: TextStyle(fontSize: 70, color: Colors.white))
+          ],
+        ),
+      ),
+    );
+  }
+}
