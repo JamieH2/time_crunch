f04b5d9f094628d31fafb49997129a1d3795d5b9
megabrokeboi <daniel.c.j.wood@gmail.com>
2023-03-31 13:15:10 +0100

    Update email_page.dart

--- lib/screens/email_page.dart
+++ lib/screens/email_page.dart
@@ -2,9 +2,14 @@
 import 'settings_page.dart';
 import 'package:flutter/material.dart';
 
-class EmailPage extends StatelessWidget {
+class EmailPage extends StatefulWidget {
   const EmailPage({Key? key}) : super(key: key);
 
+  @override
+  _EmailPageState createState() => _EmailPageState();
+}
+
+class _EmailPageState extends State<EmailPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
